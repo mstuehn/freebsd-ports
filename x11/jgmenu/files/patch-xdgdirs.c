596af4eb10771e1d55bdc5855257a6a763114ac6
Manuel Stühn <freebsd@justmail.de>
2019-01-11 08:26:30 +0100

    update jgmenu to v1.6

--- x11/jgmenu/files/patch-xdgdirs.c
+++ x11/jgmenu/files/patch-xdgdirs.c
@@ -1,12 +1,10 @@
---- xdgdirs.c.orig	2018-11-07 17:39:07 UTC
+--- xdgdirs.c.orig	2019-01-11 07:20:44 UTC
 +++ xdgdirs.c
-@@ -23,7 +23,8 @@ static char *xdg_base_dirs[] = {
- };
+@@ -24,6 +24,7 @@ static char *xdg_base_dirs[] = {
  
  static char *xdg_config_dirs[] = {
--	"$XDG_CONFIG_HOME", "$HOME/.config", "$XDG_CONFIG_DIRS", "/etc/xdg",
-+	"$XDG_CONFIG_HOME", "$HOME/.config", "$XDG_CONFIG_DIRS", 
-+	"/usr/local/etc/xdg", "/etc/xdg",
+ 	"$XDG_CONFIG_HOME", "$HOME/.config", "$XDG_CONFIG_DIRS", "/etc/xdg",
++	"/usr/local/etc/xdg",
  	NULL
  };
  
